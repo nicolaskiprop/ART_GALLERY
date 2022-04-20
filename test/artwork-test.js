ca2fe7d65fdc99a158c9fe9efb8de2b57de48d95
nicolskiprop <yegonicholas@gmail.com>
2022-04-20 22:46:37 +0300

    test

---
+++ test/artwork-test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+const { isCallTrace } = require('hardhat/internal/hardhat-network/stack-traces/message-trace');
+
+
+describe("Artwork Smart Contract Tests", function() {
+    this.beforeEach(async function() {
+
+    })
+    it("NFT is Minted successfully", async function() {
+
+    })
+
+    it("tokenURI is set successfully", async function() {
+
+    })
+    
+})
